44b3d8db310ff05859a3c00744a419f67078adac
Lucio D'Alessandro <lda@juxt.pro>
2023-08-28 14:10:38 +0200

    use display browser

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
+      manifest: {
+        display: 'browser'
+      },
       workbox: {
         runtimeCaching: [
           {
