5155c385ba7e367d0ecac143318d0bcf14cd4007
Lucio D'Alessandro <lda@juxt.pro>
2024-05-02 02:04:19 +0200

    Go back to App name

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App2";
+import { App } from "./App";
 import "./app.css";
 
 const rootEl = document.getElementById("app");
