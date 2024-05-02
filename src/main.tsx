db7d58c6dad6630dfef168aeb91205f48a3e17d3
Lucio D'Alessandro <lda@juxt.pro>
2024-05-02 02:01:45 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { App } from "./App2";
 import "./app.css";
 
 const rootEl = document.getElementById("app");
