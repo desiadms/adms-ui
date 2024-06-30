acaa5f8d1312d1bf4a6198ac0bb73de9f56c71a3
lukemuehring <muehring.luke@gmail.com>
2024-06-30 13:46:08 -0600

    add prefetch for QR Scanner to support offline

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,30 @@ import { Toaster } from "react-hot-toast";
 import { router } from "./router";
 import { nhost } from "./hooks";
 
+const registerPrefetchQRScanServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        `${window.location.origin}/PrefetchQrScanServiceWorker.js`,
+        {
+          scope: "/",
+        },
+      );
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
+registerPrefetchQRScanServiceWorker();
+
 export function App() {
   return (
     <StrictMode>
